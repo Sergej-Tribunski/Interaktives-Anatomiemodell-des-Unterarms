0efdbb090187ca929a83f17d97f106eea4fa6f05
298764061994ASDFserg <str49168@stud.hs-furtwangen.de>
2026-05-04 16:42:56 +0200

    Added shader and Lightsource

--- Skelett/Script/Build/Script.js
+++ Skelett/Script/Build/Script.js
@@ -1,1 +1,75 @@
-console.info("%cTo start scripting, run the typescript compiler in PROJECTFOLDER/Script/Source/. This is also where your code should be. Use any IDE you like.", "color: green; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static { this.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript); }
+        constructor() {
+            super();
+            // Properties may be mutated by users in the editor via the automatically created user interface
+            this.message = "CustomComponentScript added to ";
+            // Activate the functions of this component as response to events
+            this.hndEvent = (_event) => {
+                switch (_event.type) {
+                    case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                        ƒ.Debug.log(this.message, this.node);
+                        break;
+                    case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                        this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                        this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                        break;
+                    case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                        // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                        break;
+                }
+            };
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+        }
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let middleFinger = null;
+    function start(_event) {
+        viewport = _event.detail;
+        viewport.getBranch();
+        let branch = viewport.getBranch();
+        for (let node of branch.getIterator(true))
+            if (node.name.startsWith("Proximal phalanx of third finger")) {
+                middleFinger = node;
+                break;
+            }
+        console.log(middleFinger);
+        let material = ƒ.Project.getResourcesByName("MaterialShaderGouraud")[0];
+        for (let node of branch.getIterator(true))
+            if (node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1")) {
+                //console.log("Node " + node.name + " has the components:")
+                let cmpMaterial = node.getComponent(ƒ.ComponentMaterial);
+                cmpMaterial.material = material;
+            }
+        middleFinger?.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        ƒ.Physics.simulate(); // if physics is included and used
+        middleFinger?.mtxLocal.rotateX(1);
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+})(Script || (Script = {}));
+//Proximal phalanx of third finger of hand.l
+//# sourceMappingURL=Script.js.map
--- Skelett/Script/Source/Main.ts
+++ Skelett/Script/Source/Main.ts
@@ -4,17 +4,41 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  let middleFinger: ƒ.Node | null = null;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    viewport.getBranch();
+    let branch: ƒ.Node = viewport.getBranch();
+
+    for (let node of branch.getIterator(true))
+      if (node.name.startsWith("Proximal phalanx of third finger")) {
+        middleFinger = node;
+        break;
+      }
+    console.log(middleFinger);
+
+    let material: ƒ.Material = <ƒ.Material>ƒ.Project.getResourcesByName("MaterialShaderGouraud")[0];
+
+    for (let node of branch.getIterator(true))
+      if (node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1")) {
+        //console.log("Node " + node.name + " has the components:")
+        let cmpMaterial:ƒ.ComponentMaterial = node.getComponent(ƒ.ComponentMaterial);
+        cmpMaterial.material = material;
+      }
+
+    middleFinger?.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
-    // ƒ.Physics.simulate();  // if physics is included and used
+    ƒ.Physics.simulate();  // if physics is included and used
+    middleFinger?.mtxLocal.rotateX(1);
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
 }
+//Proximal phalanx of third finger of hand.l
