6e4be359943d708674de463192bec075b5ed4953
298764061994ASDFserg <str49168@stud.hs-furtwangen.de>
2026-05-21 23:04:41 +0200

    Remove old code from Main.ts for better overview. Add Joint and rotate one bone around another.

--- Skelett/Script/Build/Script.js
+++ Skelett/Script/Build/Script.js
@@ -42,129 +42,57 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
-    //let middleFinger: ƒ.Node | null = null;
-    /* let secondDistal: ƒ.Node | null = null;
-    let secondMiddle: ƒ.Node | null = null;
-    let secondProximal: ƒ.Node | null = null;
-    let secondMetacarpal: ƒ.Node | null = null; */
-    /*let rbSecondDistal: ƒ.ComponentRigidbody = new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE);
-     let rbSecondMiddle: ƒ.ComponentRigidbody | null = null;
-    let rbSecondProximal: ƒ.ComponentRigidbody | null = null;
-    let rbSecondMetacarpal: ƒ.ComponentRigidbody | null = null; */
+    let distal = null;
+    let middle = null;
+    let rbDistal = null;
+    let rbMiddle = null;
+    let joint = null;
+    let rotVector = new ƒ.Vector3(-0.1, 0, 0);
+    let minRotation = 0;
+    let maxRotation = -90;
     function start(_event) {
         viewport = _event.detail;
-        /* viewport.camera.mtxPivot.translateX(-0.26);
-        viewport.camera.mtxPivot.translateY(0.7);
-        viewport.camera.mtxPivot.translateZ(4.7); */
         viewport.getBranch();
         let branch = viewport.getBranch();
-        //viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.JOINTS_AND_COLLIDER;
+        viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.JOINTS_AND_COLLIDER;
         for (let node of branch.getIterator(true)) {
             if (node.name.startsWith("Arrow"))
                 node.activate(false);
         }
-        /* for (let node of branch.getIterator(true))
-          if (node.name.startsWith("Proximal phalanx of third finger")) {
-            middleFinger = node;
-            break;
-          }
-        console.log(middleFinger);
-        middleFinger?.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE)); */
-        /* for (let node of branch.getIterator(true)) {
-          if (node.name.startsWith("Distal phalanx of second finger") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-            secondDistal = node;
-          if (node.name.startsWith("Middle phalanx of second finger") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-            secondMiddle = node;
-          if (node.name.startsWith("Proximal phalanx of second finger") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-            secondProximal = node;
-          if (node.name.startsWith("Second metacarpal") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-            secondMetacarpal = node;
-        }
-        secondDistal?.addComponent(rbSecondDistal); */
-        /*     secondMetacarpal!.addChild(secondProximal!);
-            secondProximal!.addChild(secondMiddle!);
-            secondMiddle!.addChild(secondDistal!); */
-        /* secondDistal?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-        secondMiddle?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-        secondProximal?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-        secondMetacarpal?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.STATIC, ƒ.COLLIDER_TYPE.SPHERE));
-    
-        let rbMetacarpal = secondMetacarpal?.getComponent(ƒ.ComponentRigidbody);
-        rbMetacarpal?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-        let rbProximal = secondProximal?.getComponent(ƒ.ComponentRigidbody);
-        rbProximal?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-        let rbMiddle = secondMiddle?.getComponent(ƒ.ComponentRigidbody);
-        rbMiddle?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-        let rbDistal = secondDistal?.getComponent(ƒ.ComponentRigidbody);
-        rbDistal?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-     */
-        /* if (rbMetacarpal && rbProximal) {
-          let joint = new ƒ.JointRevolute(
-            rbMetacarpal,
-            rbProximal,
-            ƒ.Vector3.X()
-          );
-          joint.anchor = new ƒ.Vector3(-0.285, -0.76, -0.04);
-          secondMetacarpal?.addComponent(joint);
-        }
-        if (rbProximal && rbMiddle){
-          let joint = new ƒ.JointRevolute(
-            rbProximal,
-            rbMiddle,
-            ƒ.Vector3.X()
-          );
-          joint.anchor = new ƒ.Vector3(-0.315, -0.727, -0.063);
-          secondProximal?.addComponent(joint);
-        }
-        if (rbMiddle && rbDistal){
-          let joint = new ƒ.JointRevolute(
-            rbMiddle,
-            rbDistal,
-            ƒ.Vector3.X()
-          );
-          joint.anchor = new ƒ.Vector3(-0.315, -0.727, -0.063);
-          secondMiddle?.addComponent(joint);
-        } */
-        /* for (let node of branch.getIterator(true)) {
-          console.log(node.name);
-        } */
-        /* for (let node of branch.getIterator(true)) {
-          if (!(node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1") || node.name.startsWith("Arrow"))) {
-            node.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-            console.log("Node " + node.name + " has the Components:");
-            console.log(node.getAllComponents());
-    
-    
-          }
-          if (node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1")) {
-            console.log("Child Node " + node.name + " has the Components:");
-            console.log(node.getAllComponents());
-          }
-        } */
         for (let node of branch.getIterator(false)) {
-            if (!(node.name.includes("Main") || node.name.includes("Scene") || node.name.includes("Focus") || node.name.includes("Arrow") || node.name.includes("Primitive"))) {
-                if (node.name.endsWith("Humerus.l")) {
-                    console.log("Before: ");
-                    console.log("Humerus children: " + node.getChildren());
-                    console.log(node.getAllComponents());
-                }
-                /* let pos: ƒ.Vector3 = node.mtxLocal.translation.clone;
-                let rot: ƒ.Vector3 = node.mtxLocal.rotation.clone;
-                let scl: ƒ.Vector3 = node.mtxLocal.scaling.clone; */
-                node.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-                //node.getComponent(ƒ.ComponentRigidbody).initialization = ƒ.BODY_INIT.TO_NODE;
-                /* node.getComponent(ƒ.ComponentRigidbody).isInitialized = false;
-                node.getComponent(ƒ.ComponentRigidbody).initialize(); */
-                /* node.getComponent(ƒ.ComponentRigidbody).setScaling(scl);
-                node.getComponent(ƒ.ComponentRigidbody).setPosition(pos);
-                node.getComponent(ƒ.ComponentRigidbody).setRotation(rot); */
-                if (node.name.endsWith("Humerus.l")) {
-                    console.log("After: " + node.getAllComponents());
-                    console.log("Humerus children: " + node.getChildren());
-                    console.log(node.getAllComponents());
-                }
+            if (node.name.includes("Middle phalanx of second") && !node.name.includes("Primitive")) {
+                middle = node;
+            }
+            if (node.name.includes("Distal phalanx of second") && !node.name.includes("Primitive")) {
+                distal = node;
+            }
+            if (middle && distal) {
+                break;
             }
         }
+        rbMiddle = new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.STATIC, ƒ.COLLIDER_TYPE.SPHERE);
+        rbMiddle.mtxPivot.scale(new ƒ.Vector3(0.005, 0.005, 0.005));
+        rbDistal = new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE);
+        rbDistal.mtxPivot.scale(new ƒ.Vector3(0.005, 0.005, 0.005));
+        if (rbMiddle && rbDistal) {
+            let axis = middle.mtxLocal.getX();
+            let localAnchor = new ƒ.Vector3((distal?.mtxWorld.translation.x - middle?.mtxWorld.translation.x) / 1.9, (distal?.mtxWorld.translation.y - middle?.mtxWorld.translation.y) / 1.9, (distal?.mtxWorld.translation.z - middle?.mtxWorld.translation.z) / 1.9);
+            axis.normalize();
+            console.log("Axis : ", axis);
+            joint = new ƒ.JointRevolute(rbMiddle, rbDistal, axis, localAnchor);
+            joint.minMotor = maxRotation;
+            joint.maxMotor = minRotation;
+        }
+        if (rbDistal && rbMiddle && middle && joint) {
+            middle.addComponent(rbMiddle);
+            distal?.addComponent(rbDistal);
+            middle.addComponent(joint);
+        }
+        ƒ.Render.prepare(branch);
+        console.log(distal?.name);
+        console.log(distal?.getAllComponents());
+        console.log(middle?.name);
+        console.log(middle?.getAllComponents());
         let material = ƒ.Project.getResourcesByName("MaterialShaderGouraud")[0];
         for (let node of branch.getIterator(false))
             if (node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1")) {
@@ -172,17 +100,23 @@ var Script;
                 cmpMaterial.material = material;
             }
         /* for (let node of branch.getIterator(false)) {
-          console.log("Node " + node.name + " had the Components:");
+          console.log("Node " + node.name + " has the Components:");
           console.log(node.getAllComponents());
         } */
+        console.log("Middle: ", middle?.mtxLocal.translation);
+        console.log("Distal: ", distal?.mtxLocal.translation);
         ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         ƒ.Physics.simulate(); // if physics is included and used
-        //middleFinger?.mtxLocal.rotateX(1);
-        //secondProximal?.mtxLocal.rotateX(1);
-        //secondDistal?.mtxLocal.rotateX(1);
+        rbDistal?.setAngularVelocity(rotVector);
+        if (rbDistal?.getRotation().x <= maxRotation) {
+            rotVector.set(0.1, 0, 0);
+        }
+        if (rbDistal?.getRotation().x >= minRotation) {
+            rotVector.set(-0.1, 0, 0);
+        }
         viewport.draw();
         ƒ.AudioManager.default.update();
     }
--- Skelett/Script/Source/Main.ts
+++ Skelett/Script/Source/Main.ts
@@ -5,31 +5,22 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
-  //let middleFinger: ƒ.Node | null = null;
-
-  /* let secondDistal: ƒ.Node | null = null;
-  let secondMiddle: ƒ.Node | null = null;
-  let secondProximal: ƒ.Node | null = null;
-  let secondMetacarpal: ƒ.Node | null = null; */
-
-  /*let rbSecondDistal: ƒ.ComponentRigidbody = new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE);
-   let rbSecondMiddle: ƒ.ComponentRigidbody | null = null;
-  let rbSecondProximal: ƒ.ComponentRigidbody | null = null;
-  let rbSecondMetacarpal: ƒ.ComponentRigidbody | null = null; */
+  let distal: ƒ.Node | null = null;
+  let middle: ƒ.Node | null = null;
+  let rbDistal: ƒ.ComponentRigidbody | null = null;
+  let rbMiddle: ƒ.ComponentRigidbody | null = null;
+  let joint: ƒ.JointRevolute | null = null;
+  let rotVector : ƒ.Vector3 = new ƒ.Vector3(-0.1,0,0);
+  let minRotation: number = 0;
+  let maxRotation: number = -90;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
-    /* viewport.camera.mtxPivot.translateX(-0.26);
-    viewport.camera.mtxPivot.translateY(0.7);
-    viewport.camera.mtxPivot.translateZ(4.7); */
-
-
-
     viewport.getBranch();
     let branch: ƒ.Node = viewport.getBranch();
 
-    //viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.JOINTS_AND_COLLIDER;
+    viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.JOINTS_AND_COLLIDER;
 
     for (let node of branch.getIterator(true)) {
       if (node.name.startsWith("Arrow"))
@@ -38,121 +29,55 @@ namespace Script {
 
 
 
-
-    /* for (let node of branch.getIterator(true))
-      if (node.name.startsWith("Proximal phalanx of third finger")) {
-        middleFinger = node;
+    for (let node of branch.getIterator(false)) {
+      if (node.name.includes("Middle phalanx of second") && !node.name.includes("Primitive")) {
+        middle = node;
+      }
+      if (node.name.includes("Distal phalanx of second") && !node.name.includes("Primitive")) {
+        distal = node;
+      }
+      if (middle && distal){
         break;
       }
-    console.log(middleFinger);
-    middleFinger?.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE)); */
-
-
-    /* for (let node of branch.getIterator(true)) {
-      if (node.name.startsWith("Distal phalanx of second finger") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-        secondDistal = node;
-      if (node.name.startsWith("Middle phalanx of second finger") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-        secondMiddle = node;
-      if (node.name.startsWith("Proximal phalanx of second finger") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-        secondProximal = node;
-      if (node.name.startsWith("Second metacarpal") && !node.name.endsWith("Primitive0") && !node.name.endsWith("Primitive1"))
-        secondMetacarpal = node;
     }
-    secondDistal?.addComponent(rbSecondDistal); */
+    rbMiddle = new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.STATIC, ƒ.COLLIDER_TYPE.SPHERE);
+    rbMiddle.mtxPivot.scale(new ƒ.Vector3(0.005, 0.005, 0.005));
+    rbDistal = new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE);
+    rbDistal.mtxPivot.scale(new ƒ.Vector3(0.005, 0.005, 0.005));
 
-
-    /*     secondMetacarpal!.addChild(secondProximal!);
-        secondProximal!.addChild(secondMiddle!);
-        secondMiddle!.addChild(secondDistal!); */
-
-    /* secondDistal?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-    secondMiddle?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-    secondProximal?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-    secondMetacarpal?.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.STATIC, ƒ.COLLIDER_TYPE.SPHERE));
-
-    let rbMetacarpal = secondMetacarpal?.getComponent(ƒ.ComponentRigidbody);
-    rbMetacarpal?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-    let rbProximal = secondProximal?.getComponent(ƒ.ComponentRigidbody);
-    rbProximal?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-    let rbMiddle = secondMiddle?.getComponent(ƒ.ComponentRigidbody);
-    rbMiddle?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
-    let rbDistal = secondDistal?.getComponent(ƒ.ComponentRigidbody);
-    rbDistal?.mtxPivot.scale(new ƒ.Vector3(0.01, 0.01, 0.01));
- */
-    /* if (rbMetacarpal && rbProximal) {
-      let joint = new ƒ.JointRevolute(
-        rbMetacarpal,
-        rbProximal,
-        ƒ.Vector3.X()
-      );
-      joint.anchor = new ƒ.Vector3(-0.285, -0.76, -0.04);
-      secondMetacarpal?.addComponent(joint);
-    }
-    if (rbProximal && rbMiddle){
-      let joint = new ƒ.JointRevolute(
-        rbProximal,
-        rbMiddle,
-        ƒ.Vector3.X()
-      );
-      joint.anchor = new ƒ.Vector3(-0.315, -0.727, -0.063);
-      secondProximal?.addComponent(joint);
-    }
     if (rbMiddle && rbDistal){
-      let joint = new ƒ.JointRevolute(
+      let axis: ƒ.Vector3 = middle!.mtxLocal.getX();
+      let localAnchor: ƒ.Vector3 = new ƒ.Vector3(
+        (distal?.mtxWorld.translation.x! - middle?.mtxWorld.translation.x!)/1.9,
+        (distal?.mtxWorld.translation.y! - middle?.mtxWorld.translation.y!)/1.9,
+        (distal?.mtxWorld.translation.z! - middle?.mtxWorld.translation.z!)/1.9
+      );
+        axis.normalize();
+        console.log("Axis : ", axis);
+      joint = new ƒ.JointRevolute(
         rbMiddle,
         rbDistal,
-        ƒ.Vector3.X()
+        axis,
+        localAnchor
       );
-      joint.anchor = new ƒ.Vector3(-0.315, -0.727, -0.063);
-      secondMiddle?.addComponent(joint);
-    } */
-
-    /* for (let node of branch.getIterator(true)) {
-      console.log(node.name);
-    } */
-
-
-
-    /* for (let node of branch.getIterator(true)) {
-      if (!(node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1") || node.name.startsWith("Arrow"))) {
-        node.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-        console.log("Node " + node.name + " has the Components:");
-        console.log(node.getAllComponents());
-
-
-      }
-      if (node.name.endsWith("Primitive0") || node.name.endsWith("Primitive1")) {
-        console.log("Child Node " + node.name + " has the Components:");
-        console.log(node.getAllComponents());
-      }
-    } */
-
-    for (let node of branch.getIterator(false)) {
-      if (!(node.name.includes("Main") || node.name.includes("Scene") || node.name.includes("Focus") || node.name.includes("Arrow") || node.name.includes("Primitive"))) {
-        if (node.name.endsWith("Humerus.l")){
-          console.log("Before: ");
-          console.log("Humerus children: " + node.getChildren());
-          console.log(node.getAllComponents());
-        }
-        /* let pos: ƒ.Vector3 = node.mtxLocal.translation.clone;
-        let rot: ƒ.Vector3 = node.mtxLocal.rotation.clone;
-        let scl: ƒ.Vector3 = node.mtxLocal.scaling.clone; */
-        node.addComponent(new ƒ.ComponentRigidbody(0, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.SPHERE));
-        //node.getComponent(ƒ.ComponentRigidbody).initialization = ƒ.BODY_INIT.TO_NODE;
-        /* node.getComponent(ƒ.ComponentRigidbody).isInitialized = false;
-        node.getComponent(ƒ.ComponentRigidbody).initialize(); */
-
-        /* node.getComponent(ƒ.ComponentRigidbody).setScaling(scl);
-        node.getComponent(ƒ.ComponentRigidbody).setPosition(pos);
-        node.getComponent(ƒ.ComponentRigidbody).setRotation(rot); */
-
-        if (node.name.endsWith("Humerus.l")){
-          console.log("After: " + node.getAllComponents());
-          console.log("Humerus children: " + node.getChildren());
-          console.log(node.getAllComponents());
-        }
-      }
+      joint.minMotor = maxRotation;
+      joint.maxMotor = minRotation;
     }
+
+
+
+    if (rbDistal && rbMiddle && middle && joint){
+      middle.addComponent(rbMiddle);
+      distal?.addComponent(rbDistal);
+      middle.addComponent(joint);
+    }
+    ƒ.Render.prepare(branch);
+
+    console.log(distal?.name);
+    console.log(distal?.getAllComponents());
+    console.log(middle?.name);
+    console.log(middle?.getAllComponents());
+
 
     let material: ƒ.Material = <ƒ.Material>ƒ.Project.getResourcesByName("MaterialShaderGouraud")[0];
     for (let node of branch.getIterator(false))
@@ -162,18 +87,27 @@ namespace Script {
       }
 
     /* for (let node of branch.getIterator(false)) {
-      console.log("Node " + node.name + " had the Components:");
+      console.log("Node " + node.name + " has the Components:");
       console.log(node.getAllComponents());
     } */
+   console.log("Middle: ", middle?.mtxLocal.translation);
+   console.log("Distal: ", distal?.mtxLocal.translation);
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     ƒ.Physics.simulate();  // if physics is included and used
-    //middleFinger?.mtxLocal.rotateX(1);
-    //secondProximal?.mtxLocal.rotateX(1);
-    //secondDistal?.mtxLocal.rotateX(1);
+
+    rbDistal?.setAngularVelocity(rotVector);
+    if(rbDistal?.getRotation().x! <= maxRotation){
+      rotVector.set(0.1,0,0);
+    }
+    if(rbDistal?.getRotation().x! >= minRotation){
+      rotVector.set(-0.1,0,0);
+    }
+    
 
     viewport.draw();
     ƒ.AudioManager.default.update();
